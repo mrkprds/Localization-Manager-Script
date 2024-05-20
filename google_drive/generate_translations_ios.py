c26335d74d9bc286d18826207bd1fed66fab029a
Patrick Perdon <markpatrickperdon@gmail.com>
2024-05-20 17:54:57 +0800

    fix json structure

--- google_drive/generate_translations_ios.py
+++ google_drive/generate_translations_ios.py
@@ -9,7 +9,11 @@ import json
 class IOSTranslation(GenerateTranslation):
 
     def generate(self):
-        dictionary = {}
+        dictionary = {
+            "sourceLanguage": "",
+            "strings": {
+            },
+        }
 
         for sheet in self.sheets:
             for column_index, column_value in enumerate(sheet.columns):
@@ -17,7 +21,9 @@ class IOSTranslation(GenerateTranslation):
                     dictionary["sourceLanguage"] = column_value.language_code
 
                     for localized_string in column_value.strings:
-                        dictionary[localized_string.localized_key] = {}
+                        dictionary["strings"].update({
+                            localized_string.localized_key: {}
+                        })
                 else:
                     for localized_string in column_value.strings:
 
@@ -28,28 +34,31 @@ class IOSTranslation(GenerateTranslation):
                         if localized_string.localized_value.startswith("//"):
                             continue
 
-                        dictionary[localized_string.localized_key] = {
-                            "localizations": {
-                                column_value.language_code: {
-                                    "stringUnit": {
-                                        "state": "translated",
-                                        "value": localized_string.localized_value
+                        dictionary["strings"][localized_string.localized_key].update({
+                                "localizations": {
+                                    column_value.language_code: {
+                                        "stringUnit": {
+                                            "state": "translated",
+                                            "value": localized_string.localized_value
+                                        }
                                     }
                                 }
                             }
-                        }
+                        )
 
                         # Append Comments on Localization String (if there's any)
                         if not localized_string.comment:
                             continue
 
-                        dictionary[localized_string.localized_key]["comment"] = localized_string.comment
+                        dictionary["strings"][localized_string.localized_key].update({
+                            "comment": localized_string.comment
+                        })
 
         dictionary["version"] = "1.0"
 
         FileManager.save_to_google_drive(
             drive_service=build('drive', 'v3', credentials=self._credentials),
-            file=json.dumps(dictionary),
+            file=json.dumps(dictionary, ensure_ascii=False, indent=2),
             file_name="Localizable.json",
             mime_type="application/json",
             folder_structure=['Translations', 'iOS']
