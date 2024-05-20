fb7a1f9c75e513632c3098927a11be811c72e9f7
Patrick Perdon <markpatrickperdon@gmail.com>
2024-05-20 10:47:22 +0800

    implement the intial main method setting up the google app script API

---
+++ google_drive/generate_translations_main.py
@@ -0,0 +1,36 @@
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
+
+# Additional Constants
+credential_file = "client_secret_505442340141-8o06brqss1an7qm46hnvrsqssoh8latg.apps.googleusercontent.com.json"
+
+
+def main():
+    """Calls the Apps Script API."""
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                credential_file, SCOPES
+            )
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+
+
+main()
